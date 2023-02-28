8ed15ec47c5b10426ba382e3149408ae3a15b703
Akshay Khona <desertsled800c@gmail.com>
2023-02-28 11:40:49 -0600

    Phase-8

--- GuessingGames/Program.cs
+++ GuessingGames/Program.cs
@@ -5,64 +5,90 @@ int SecretNum = r.Next(1, 100);
 
 NumOfPrompts();
 
-void NumberPrompt(string question, int secretNum, int numOfPrompts)
+void TheGame(string question, int secretNum, int numOfPrompts)
 {
     Console.WriteLine(secretNum);
     Console.WriteLine("");
-    for (int i = 0; i < numOfPrompts; i++)
+    if (numOfPrompts == -1)
     {
-        Console.WriteLine($"{question}");
-        string answer = Console.ReadLine();
-        int parsedAnswer = int.Parse(answer);
-        if (parsedAnswer == secretNum)
+        while (true)
         {
-            Console.WriteLine("YOU WIN");
-            return;
-        }
-        else
-        {
-            if (parsedAnswer < secretNum)
+            Console.WriteLine($"{question}");
+            string answer = Console.ReadLine();
+            int parsedAnswer = int.Parse(answer);
+            if (parsedAnswer == secretNum)
             {
-                Console.WriteLine("Too Low");
+                Console.WriteLine("YOU WIN");
+                return;
             }
             else
             {
-                Console.WriteLine("Too High");
+                if (parsedAnswer < secretNum)
+                {
+                    Console.WriteLine("Too Low");
+                }
+                else
+                {
+                    Console.WriteLine("Too High");
+                }
+                Console.WriteLine("");
             }
-            Console.WriteLine("");
-            Console.WriteLine($"You have {numOfPrompts - (i + 1)} guesses remaining");
+        }
+    }
+    else
+    {
+        for (int i = 0; i < numOfPrompts; i++)
+        {
+            Console.WriteLine($"{question}");
+            int answer = int.Parse(Console.ReadLine());
+            if (answer == secretNum)
+            {
+                Console.WriteLine("YOU WIN");
+                return;
+            }
+            else
+            {
+                if (answer < secretNum)
+                {
+                    Console.WriteLine("Too Low");
+                }
+                else
+                {
+                    Console.WriteLine("Too High");
+                }
+                Console.WriteLine("");
+                Console.WriteLine($"You have {numOfPrompts - (i + 1)} guesses remaining");
+            }
         }
     }
 }
 
 void NumOfPrompts()
 {
-    Console.WriteLine($"What difficulty level would you like to play? (Easy | Medium | Hard):");
+    Console.WriteLine($"What difficulty level would you like to play? (Easy | Medium | Hard | Cheater):");
     string difficulty = Console.ReadLine().ToLower();
     Console.WriteLine($"Then {difficulty.ToUpper()} you will get.");
     Console.WriteLine("");
     if (difficulty == "easy")
     {
-        NumberPrompt("Guess a Number??", SecretNum, 8);
+        TheGame("Guess a Number??", SecretNum, 8);
     }
     else if (difficulty == "medium")
     {
-        NumberPrompt("Guess a Number??", SecretNum, 6);
+        TheGame("Guess a Number??", SecretNum, 6);
     }
     else if (difficulty == "hard")
     {
-        NumberPrompt("Guess a Number??", SecretNum, 4);
+        TheGame("Guess a Number??", SecretNum, 4);
+    }
+    else if (difficulty == "cheater")
+    {
+        TheGame("Guess a Number??", SecretNum, -1);
     }
     else
     {
         Console.WriteLine("No such category, check you spelling!!");
     }
 }
-
-
-
-
-
-
 
 
